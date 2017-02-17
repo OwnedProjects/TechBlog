d307bc12578e5436d607099b69af63407b14381c
Avees Panchbhai <Avees.Panchbhai@splus.local>
2017-02-17 18:17:04 +0530

    add comments!

--- template/Blog-details/js/controller/BlogDetsController.js
+++ template/Blog-details/js/controller/BlogDetsController.js
@@ -2,16 +2,18 @@ angular.module("TechBlog")
 	.controller("BlogDetsController", BlogDetsController);
 
 function BlogDetsController(){
+    // variables defined
     var vm = this;
     vm.showcommentSection = false;
-    vm.togglecommentSection = togglecommentSection;
+
+    // function defining
     vm.init = init;
+    vm.togglecommentSection = togglecommentSection;
     
     function init(){
     }
 
     function togglecommentSection (){
-        alert('sdfsd');
         vm.showcommentSection = vm.showcommentSection === false ? true: false;
     }
     
