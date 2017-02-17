33a2fd3a212308f2e5fe101b9c6ca510d7630070
Avees Panchbhai <Avees.Panchbhai@splus.local>
2017-02-17 11:59:41 +0530

    file updated with new changes!

--- template/Add-blog/js/controller/AddBlogController.js
+++ template/Add-blog/js/controller/AddBlogController.js
@@ -3,4 +3,18 @@ angular.module("TechBlog")
 
 function AddBlogController(){
     var vm = this;
+    vm.showTitleInst = false;
+    vm.showFormatInst = false;
+    vm.showTagInst = false;
+
+    vm.init = function(){
+        
+    }
+
+    vm.showInstruction = function(a){
+        a = true;
+    }
+
+    vm.init();
+
 }
