d2136d8eed83926828e636ccab10467db2b5be9a
wasimakram-mulla <wasim3ace@gmail.com>
2017-02-19 00:28:29 +0530

    Major Conflict in AllBlogs

--- template/All-blogs/js/controller/AllBlogController.js
+++ template/All-blogs/js/controller/AllBlogController.js
@@ -21,12 +21,17 @@ function AllBlogController($firebaseArray, $rootScope, $location){
             vm.users = $firebaseArray(vm.usersRef);
             vm.currentUID = user.uid;
 <<<<<<< HEAD
+<<<<<<< HEAD
             //console.log(vm.currentUID);
             vm.findUserRootRef = firebase.database()
                     .ref().child('/users')
                     .orderByChild('uid')
                     .equalTo(vm.currentUID)
                     .once('value')
+=======
+            console.log(vm.currentUID);
+            vm.findUserRootRef = firebase.database().ref().child('/users').orderByChild('uid').equalTo(vm.currentUID).once('value')
+>>>>>>> 01aca62ed69b5635587e47061cddfa52e95121a9
 =======
             console.log(vm.currentUID);
             vm.findUserRootRef = firebase.database().ref().child('/users').orderByChild('uid').equalTo(vm.currentUID).once('value')
