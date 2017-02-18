da9b4c068100a7b39aeb7bd4343a44520c8ab774
wasimakram-mulla <wasim3ace@gmail.com>
2017-02-19 01:01:40 +0530

    working on Storage add blog- Pending

--- template/Add-blog/js/controller/AddBlogController.js
+++ template/Add-blog/js/controller/AddBlogController.js
@@ -1,13 +1,17 @@
 angular.module("TechBlog")
 	.controller("AddBlogController", AddBlogController);
 
-AddBlogController.$inject = ["$rootScope", "$location"];
+AddBlogController.$inject = ["$firebaseArray", "$rootScope", "$location"];
 
-function AddBlogController($rootScope){
+function AddBlogController($firebaseArray, $rootScope, $location){
     var vm = this;
     vm.showTitleInst = false;
     vm.showFormatInst = false;
     vm.showTagInst = false;
+    vm.rootRef = null;
+    vm.blogs = null;
+    vm.rootRefBlogLinks = null;
+    vm.bloglinks = null;
     vm.init = init;
     vm.createNewBlog = createNewBlog;
 
@@ -15,7 +19,15 @@ function AddBlogController($rootScope){
         $rootScope.setActiveMenu();
     }
     vm.init = function(){
-       console.log("AddBlogController") ;
+       if(!$rootScope.user){
+           $location.path('/');
+       }
+       else{
+            vm.rootRef = firebase.database().ref().child('/blog-post');
+			vm.blogs = $firebaseArray(vm.rootRef);
+			vm.rootRefBlogLinks = firebase.database().ref().child('/blog-links');
+            vm.bloglinks = $firebaseArray(vm.rootRefBlogLinks);
+       }
     }
 
     vm.showInstruction = function(a){
@@ -23,7 +35,25 @@ function AddBlogController($rootScope){
     }
 
     function createNewBlog(){
-        console.log(vm.blogTitle, vm.content)
+        console.log(vm.blogTitle, vm.content);
+        var titleLink = vm.blogTitle.toLowerCase().split(" ").join("-");
+		var dt = new Date();
+		var postData = {
+			"bloglink":titleLink,
+			"title":vm.blogTitle,
+			"content":vm.content,
+			"blogdate": dt.getTime()
+		};
+
+		var bloglinkdata = {
+			"link": titleLink,
+			"blogTitle":vm.blogtitle,
+			"blogdate": dt.getTime()
+		};
+        console.log(postData)
+        console.log(bloglinkdata)
+		//vm.blogs.$add(postData);
+		//vm.bloglinks.$add(bloglinkdata);
     }
     vm.init();
 }
