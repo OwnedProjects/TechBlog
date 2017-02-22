b2e6ea5f89c01b17d5e2cf41a095c6fb452e9275
Avees Panchbhai <Avees.Panchbhai@splus.local>
2017-02-23 00:01:34 +0530

    my profile page tabs created!

--- template/myprofile/controller/MyprofileController.js
+++ template/myprofile/controller/MyprofileController.js
@@ -4,10 +4,23 @@ angular.module("TechBlog")
 
 function MyprofileController(){
     var vm = this;
+    vm.tab = 1;
+    vm.tab = null;
     vm.init = init;
+    vm.setTab = setTab;
+    vm.isSet = isSet;
 
     function init(){
 
     }
+
+    function setTab(newTab){
+      vm.tab = newTab;
+    };
+
+    function isSet(tabNum){
+      return vm.tab === tabNum;
+    };
+
     vm.init();
 }
