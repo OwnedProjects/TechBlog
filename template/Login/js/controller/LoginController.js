371ebfc1dd96f995301da38b7839aac825763d4c
RoundPoint <RoundPoint>
2017-04-19 22:47:59 +0530

    Added spinner on login

--- template/Login/js/controller/LoginController.js
+++ template/Login/js/controller/LoginController.js
@@ -5,25 +5,35 @@ LoginController.$inject = ["$firebaseArray", "$location", "$rootScope", "$timeou
 
 function LoginController($firebaseArray, $location, $rootScope, $timeout){
     var vm = this;
+	vm.loader = false;
     vm.init = init;
     vm.googleLogin = googleLogin;
     vm.onAuthStateChanged = onAuthStateChanged;
 
     function init(){
+		vm.loader = true;
         firebase.auth().onAuthStateChanged(vm.onAuthStateChanged);
     };
 
     function onAuthStateChanged(user){
+		vm.loader = true;
         if(user){
             //vm.currentUID = user.uid;
             $rootScope.user = user;
-             $timeout(function(){
+			$timeout(function(){
+				vm.loader = false;
                 $location.path("/allblog");
             },500);
         }
+		else{
+			$timeout(function(){
+				vm.loader = false;
+            },500);
+		}
     }
 
     function googleLogin(){
+		vm.loader = true;
         var provider = new firebase.auth.GoogleAuthProvider();
     	firebase.auth().signInWithPopup(provider).then(function(){
             firebase.auth().onAuthStateChanged(vm.onAuthStateChanged);
